2bd4b6ea726940739ab184aca957bb32dccba2d5
Hope-Shen <hsinchinshen@gmail.com>
2021-01-05 14:02:03 +0000

    Update 1_Two_Sum.py

--- LeetCode/1_Two_Sum.py
+++ LeetCode/1_Two_Sum.py
@@ -1,8 +1,21 @@
 # encoding: utf-8
 
+# 1_Two_Sum
+# 
+# Given an array of integers nums and an integer target, return indices of the two numbers such
+# that they add up to target.
+# Example 1:
 # Input: nums = [2,7,11,15], target = 9
 # Output: [0,1]
 # Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+#
+# Example 2:
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+#
+# Example 3:
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
 
 class Solution:
     # 暴力解法 O(n^2)
