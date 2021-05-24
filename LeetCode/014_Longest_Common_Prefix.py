1d2a0a15168416dde6424f642d38dd4425bebf06
Hope-Shen <hsinchinshen@gmail.com>
2021-05-24 18:22:07 +0100

    014_Longest_Common_Prefix

---
+++ LeetCode/014_Longest_Common_Prefix.py
@@ -0,0 +1,59 @@
+# encoding: utf-8
+
+# 014_Longest_Common_Prefix
+# 2021/05/12
+#
+# Write a function to find the longest common prefix string amongst an array of strings.
+# 
+# If there is no common prefix, return an empty string "".
+# 
+# Example 1:
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+# 
+# Example 2:
+# Input: strs = ["dog","racecar","car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings.
+#  
+# Constraints:
+# 1 <= strs.length <= 200
+# 0 <= strs[i].length <= 200
+# strs[i] consists of only lower-case English letters.
+
+class Solution:
+  '''
+  Vertical scanning 縱向掃描法
+  '''
+  def prefix(self, strs):
+    if not strs:
+      return ""
+
+    length, count = len(strs[0]), len(strs)
+    for i in range(length):
+      c = strs[0][i]
+      for j in range(1, count):
+        if i == len(strs[j]) or strs[j][i] != c :
+            return strs[0][:i]
+
+    return strs[0]
+
+
+if __name__ == '__main__':
+    # begin
+    s = Solution()
+    str1 = ["flower","flow","flight"]
+    print("str1:", s.prefix(str1))
+    str2 = ["dog","racecar","car"]
+    print("str2:", s.prefix(str2))
+    str3 = ["a"]
+    print("str3:", s.prefix(str3))
+    str4 = ["aa","aa","aaa"]
+    print("str4:", s.prefix(str4))
+    str5 = ["aa","aaa","a"]
+    print("str5:", s.prefix(str5))
+    str6 = ["cir","car"]
+    print("str6:", s.prefix(str6))
+    str7 = ["a","a","b"]
+    print("str7:", s.prefix(str7))
+    
---
+++ LeetCode/Note.md
@@ -0,0 +1,17 @@
+# <span style="color:LightGreen">**EASY**</span>
+
+| Problem                       | Complete Date     | 
+| :---                          |    :----:         |
+| 001_Two_Sum                   | 5th Jan.  2021    | 
+| 007_Reverse_Integer           | 7th Jan.  2021    |
+| 009_Palindrome_Number         | 7th Jan.  2021    |
+| 013_Roman_to_Integer          | 9th Jan.  2021    |
+| 014_Longest_Common_Prefix     | 12th May. 2021    |
+| 020_Valid_Parentheses         | 12th May. 2021    | 
+| 021_Merge_Two_Sorted_Lists    |                   | 
+
+
+# <span style="color:Orange">**MEDIUM**</span>
+
+| Problem                   | Complete Date     | 
+| :---                      |    :----:         |
